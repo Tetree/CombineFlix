a18f370a226f95edae43c167bcae0a4e8fa5bafd
Nuno <nunodnm@outlook.com>
2022-05-26 18:03:14 +0100

    Setup project

---
+++ Combineflix/Combineflix/Controllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Combineflix
+//
+//  Created by Nuno Mota on 26/05/2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
